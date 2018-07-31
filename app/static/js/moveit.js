da1c7f9e9ef64650361f1a0f45a48161b01f6285
redforge <ethanjmedei@gmail.com>
2018-07-30 23:06:31 -0400

    mobile fix v2

--- app/static/js/moveit.js
+++ app/static/js/moveit.js
@@ -22,6 +22,18 @@ function hideBox() {
 }
 
 function relocateBox() {
+  if (window.innerWidth > 600) {
+    relocateBoxNonMobile();
+  } else {
+    var dd = $("div.token-input-dropdown")[0];
+    $(dd).css("position", "fixed");
+    $(dd).css("bottom", "10px");
+    $(dd).css("left", "10px");
+    $(dd).css("width", "95%");
+    $(dd).css("max-width", "100%");
+  }
+}
+function relocateBoxNonMobile() {
   var ae = document.activeElement;
   var xy = getCursorXY(document.activeElement);
   var outerBox = $(ae).parent().parent();
